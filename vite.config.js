1d1e296d098a937be241a2c9889aa134177c6296
Nelson Vásquez <nvasquez@muniguate.com>
2023-11-23 12:08:46 -0600

    cambio

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base : '/GestionServicios/apps/nelson'
+  // base : '/GestionServicios/apps/{nombre de tu app separado por guiones medios}'
 })
